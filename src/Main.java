c9bf9d28f743c14170d7de5574f1a83b91b1297d
Nicholas <nicholasbuzo@gmail.com>
2026-03-23 23:38:42 -0300

    Criando repositório e adicionando primeiras anotações nele

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import S8.IntroPOO.Triangulo3Att;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangulo3Att triangulo = new Triangulo3Att();
+        triangulo.a = 10;
+        triangulo.b = 20;
+        triangulo.c = 10;
+        Triangulo3Att x = new Triangulo3Att();
+        x.a = 1;
+        x.b = 2;
+        x.c = 3;
+    }
+}
---
+++ src/S8/IntroPOO/Triangulo3Att.java
@@ -0,0 +1,10 @@
+package S8.IntroPOO;
+
+public class Triangulo3Att {
+    // Classe => tipo estruturado, contém membros (atributos e métodos)
+    // Pode conter Construtores, Sobrecarga, Heranća, Encapsulamento, Polimorfismo
+    public double a;
+    public double b;
+    public double c;
+    // public => outros arquivos podem acessar este atributo
+}
